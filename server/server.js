2de930e17ec4d0c672c51950fdd1316df6e886c0
Sebastian Hove <sebastianhove@Sebastians-MacBook-Pro.local>
2020-08-08 10:40:03 -0500

    "added basic server code"

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
 // testing upstream
+const express = require('express');
+const path = require('path');
+const app = express();
+app.get('/', (req, res) => {
+  res.end();
+});
+app.post('/', (req, res) => {
+  res.end();
+});
+module.exports = {
+  app,
+};
