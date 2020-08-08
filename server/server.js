dc43d469b443bf98f41cd95e653a1bda33160ee0
Sebastian Hove <sebastianhove@Sebastians-MacBook-Pro.local>
2020-08-08 11:41:48 -0500

    "Added basic server listener, a few modules such as nodemon, cors, bodyParser"

--- server/server.js
+++ server/server.js
@@ -2,9 +2,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require(*'cors');
+const bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.json());
+// need cors
+// body parser 
+// 
+
 app.get('/', (req, res) => {
   res.end();
 });
+
 app.post('/', (req, res) => {
   res.end();
 });
