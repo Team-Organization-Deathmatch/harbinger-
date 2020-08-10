092cdc66b85d6c303c19e696afe5c849c6a4113b
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-08-10 17:06:16 -0500

    "Webpack config to work with react"

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
 module.exports = {
+  devtool: 'eval-source-map',
   entry: './client/src/index.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './client/dist'),
+  module: {
+    rules: [
+      {
+        test: /\.(jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      }
+    ]
   },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './client/dist'
+  }
 };
