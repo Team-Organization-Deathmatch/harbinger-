e5d242fc163a071cddf71cbf8d702a31d6606bc7
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-08-10 16:59:11 -0500

    "link react to id=app"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link
+//   } from "react-router-dom";
+
+ReactDOM.render(<App />, document.getElementById('app'));  
 
 //index.jsx houses the app for webpack bundle.
