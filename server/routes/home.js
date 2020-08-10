49d3cf830634d031a11fa759e1881934f968de5e
benyoss <benyoss4@gmail.com>
2020-08-10 12:36:48 -0500

    "Added GET and POST for API routes to test DB users."

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const path = require('path');
-require('../db/database');
+const { getUsers, saveUsers } = require('../db/database');
 const homeRoute = Router();
 
 homeRoute.get('/', (req, res) => {
@@ -10,6 +10,23 @@ homeRoute.get('/', (req, res) => {
   console.log('working GET');
   res.status(200);
   res.sendFile(path.resolve("client/dist/index.html"));
+});
+
+homeRoute.get('/api/users', (req, res) => {
+  console.log("GET should hit!!");
+  getUsers().then((data) => {
+    res.status(200).send(data);
+  })
+  .catch(err => console.error(err));
+});
+
+homeRoute.post('/api/users', (req, res) => {
+console.log(req.body);
+  saveUsers(req.body).then(() => {
+    console.log('users have been saved');
+    res.end();
+  })
+  .catch(err => console.error(err));
 });
 
 //
