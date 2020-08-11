b27bf2647ffa8e8e47b8c1b1239b2de714d75258
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-08-10 20:04:38 -0500

    "added login and profile component"

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -5,3 +5,13 @@ import {
     Route,
     Link
   } from "react-router-dom";
+
+  function Login() {
+    return(
+     <div>
+       <h1>Login Component</h1>
+     </div>
+   )
+ }
+
+export default Login;
