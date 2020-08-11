1aa012c1fa5ae78d19b6c1f7e13137248c9e8462
benyoss <benyoss4@gmail.com>
2020-08-11 13:25:55 -0500

    "Rendered a basic homepage layout and search component."

--- client/src/homepage.jsx
+++ client/src/homepage.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Search from './search.jsx';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-
-  function HomePage() {
-    return(
-     <div>
-       <h1>HomePage Component</h1>
-     </div>
-   )
- }
+  BrowserRouter as Router,
+  Switch,
+  Route, 
+  Link,
+} from 'react-router-dom';
+function HomePage() {
+  return (
+    <div>
+      <h1 style={{ backgroundColor: '#800000', color: "white" }}>HomePage Component</h1>
+         <Search />
+      <h3 style={{ display: "inline-block", marginRight:"800px"}}>Top Best websites</h3>
+      <h3 style={{ display: "inline-block", textAlign: "right" }}>Top Worst websites</h3>
+    </div>
+  );
+}
 
 export default HomePage;
