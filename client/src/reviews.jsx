40c6184329c8a967f360eff35475fa4e6e1e9988
benyoss <benyoss4@gmail.com>
2020-08-11 12:39:29 -0500

    "Inserted react-hook-form module and created a basic review form for testing."

--- client/src/reviews.jsx
+++ client/src/reviews.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
-  function Reviews() {
-    return(
-     <div>
-       <h1>Reviews Component</h1>
-     </div>
-   )
- }
+function Reviews() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+  
+  return (
+    <div>
+      <form onSubmit={ handleSubmit(onSubmit) }>
+          <label>|Username|</label>
+          <input ref={ register } name="username" />
+
+          <label>|Message|</label>
+          <textarea ref={ register } name="message" />
+          <button>Submit Review</button>
+      </form>
+      <h1>Reviews Component</h1>
+    </div>
+  );
+}
 
 export default Reviews;
