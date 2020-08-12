7846545e848765610bf055cd98d3c88a1353aeb8
Sebastian Hove <sebastianhove@Sebastians-MacBook-Pro.local>
2020-08-12 15:35:07 -0500

    "Added a function to find articles based on keyword, still need to test it"

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,8 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, TableHints } = require('sequelize');
 
 // create a connection to localDB
 
-const db = new Sequelize('harbinger', 'root', '', {
+const db = new Sequelize('harbinger', 'root', 'fossil15', {
   host: 'localhost',
   dialect: 'mysql',
 });
@@ -152,37 +152,68 @@ const Keyword = db.define('Keyword', {
 });
 Keyword.sync();
 
-const saveOrFindKeyWord = (keyword) => Keyword.findOne({ where: { keyword } })
-  .then((data) => {
+// const findArticleByKeyWord = (keyword) =>
+//   Keyword.findOne({ where: { keyword } })
+//   .then((data) => {
+//     if (data === null) {
+//       console.log('keyword not found');
+//       return;
+//     } else {
+//       console.log('hello')
+//     };
+const findArticleByKeyWord = (keyword) =>
+  Keyword.findOne({ where: { keyword } }).then((data) => {
     if (data === null) {
-      console.log('keyword created!!!');
-      return Keyword.create({ keyword });
+      console.log('no keyword found');
+      return;
+    } else {
+      Review.findAll({
+        where: {
+          keyword: data.id,
+        },
+      }).then((data) => console.log(data));
+      // need to change this to return data?
     }
-    return data;
-  })
-  .catch((err) => console.log(err));
+  });
 
-const saveOrFindWebUrl = (url) => WebUrls.findOne({ where: { url } })
-  .then((data) => {
+const saveOrFindKeyWord = (keyword) =>
+  Keyword.findOne({ where: { keyword } })
+    .then((data) => {
+      if (data === null) {
+        console.log('keyword created!!!');
+        return Keyword.create({ keyword });
+      }
+      return data;
+    })
+    .catch((err) => console.log(err));
+
+const saveOrFindWebUrl = (url) =>
+  WebUrls.findOne({ where: { url } })
+    .then((data) => {
+      if (data === null) {
+        console.log('webURL created!');
+        return WebUrls.create({ url });
+      }
+      return data;
+    })
+    .catch((err) => console.log(err));
+
+const saveUsers = (username, serial, bio, image) =>
+  Users.findOne({ where: { serial } }).then((data) => {
     if (data === null) {
-      console.log('webURL created!');
-      return WebUrls.create({ url });
+      return Users.create({
+        username,
+        serial,
+        bio,
+        image,
+      });
     }
-    return data;
-  })
-  .catch((err) => console.log(err));
-
-const saveUsers = (username, serial, bio, image) => Users.findOne({ where: { serial } }).then((data) => {
-  if (data === null) {
-    return Users.create({
-      username, serial, bio, image,
-    });
-  }
-  console.log(data);
-  console.log('entry already exists');
-});
+    console.log(data);
+    console.log('entry already exists');
+  });
 
 const getUser = (id) => Users.findOne({ where: { serial: id } });
+
 const saveReview = (username, text, weburl, keyword) => {
   let idUser;
   let idWeb;
@@ -207,6 +238,7 @@ const saveReview = (username, text, weburl, keyword) => {
     });
   });
 };
+
 module.exports = {
   db,
   getUser,
