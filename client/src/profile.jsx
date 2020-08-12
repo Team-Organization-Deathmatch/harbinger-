00e5bb75b5ad3286f1f84a28f643cb6cb6dd46b3
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-11 19:21:45 -0500

    linked up profile.jsx post and '/profile' POST

--- client/src/profile.jsx
+++ client/src/profile.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-  function Profile() {
-    return(
-     <div>
-       <h1>Profile Component</h1>
-     </div>
-   )
- }
+
+function Profile() {
+  
+  const [users, setUsers] = useState([]);
+  
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (userBio) => {
+    axios.post('/profile', { bio: userBio })
+    .then(({ data }) => {
+      console.log(data);
+    })
+  };
+
+  useEffect(() => {
+    axios.get('/good')
+      .then(({ data }) => {
+        console.log(data);
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{users.username}</h1>
+      <img src={users.image} />
+      <div>{users.bio}</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Bio</label>
+        <textarea ref={ register } name="message" />
+        <button>Submit Bio</button>
+      </form>
+    </div>
+  );
+}
 
 export default Profile;
