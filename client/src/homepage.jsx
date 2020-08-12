e904022b138f5a619a66ac4f21b10865400f8d0e
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-11 19:08:45 -0500

    changed the link from profile to profile2

--- client/src/homepage.jsx
+++ client/src/homepage.jsx
@@ -16,13 +16,13 @@ function HomePage() {
       .then(({ data }) => {
         setUser(data.username);
       });
-  });
+  }, []);
 
   return (
     <div>
       <div style={{ backgroundColor: '#800000' }}>
         <h2 style={{ display: 'inline-block', color: 'white', marginRight: '800px' }}>HomePage Component</h2>
-        <Link to="/profile"><h2 style={{ display: 'inline-block', color: 'white', textAlign: 'right' }}>{user}</h2></Link>
+        <Link to="/profile2"><h2 style={{ display: 'inline-block', color: 'white', textAlign: 'right' }}>{user}</h2></Link>
       </div>
       <Search />
       <h3 style={{ display: 'inline-block', marginRight: '800px' }}>Top Best websites</h3>
