958d31204eadeaedd78e4b76695342398f256d1d
Sebastian Hove <sebastianhove@Sebastians-MacBook-Pro.local>
2020-08-12 11:43:56 -0500

    "Successfully using Axios and console logging bing response data"

--- client/src/search.jsx
+++ client/src/search.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+
+const searchBing = (query) => {
+  // may need to change query to query.website since the
+  // current click returns data that looks like { website: "amazon }
+  const data = JSON.stringify(query);
+
+  const config = {
+    method: 'post',
+    url: '/api/websites/search',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
 function Search() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = (data) => searchBing(data);
   return (
     <div>
-      <form onSubmit={ handleSubmit(onSubmit) }>
-          <label>Search:</label>
-          <input ref={ register } name="website" />
-          <button>Search websites</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Search:</label>
+        <input ref={register} name='clientSearch' />
+        <button>Search websites</button>
       </form>
     </div>
   );
