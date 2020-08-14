9e8e282045de5d3e02ce6e385a3e5f0a58e8be1d
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-08-13 19:01:05 -0500

    "added component for favorite users reviews"

---
+++ client/src/UserReviews.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function UserReviews() {
+  return (
+    <div>
+      <h1>
+        Favorite Reviews Component
+      </h1>
+    </div>
+  );
+}
+
+export default UserReviews;
