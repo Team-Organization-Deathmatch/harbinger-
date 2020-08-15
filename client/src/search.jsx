f175bfa4c0a26c0f77358ce6347bd0e8818cb0b0
benyoss <benyoss4@gmail.com>
2020-08-15 09:59:43 -0500

    "Added redir and count

--- client/src/search.jsx
+++ client/src/search.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Switch, Route, Link, Redirect,
+} from 'react-router-dom';
 import axios from 'axios';
 import { testArray } from './testData';
-
+let boom;
+let redir = '';
+let count = 0;
 function Search() {
   const searchBing = (query) => {
     const data = JSON.stringify(query);
@@ -14,19 +18,19 @@ function Search() {
       headers: {
         'Content-Type': 'application/json',
       },
-      data: data,
+      data,
     };
-    //console.log(testArray, 'hello');
-    //webSitesUpdate(testArray);
+    // console.log(testArray, 'hello');
+    // webSitesUpdate(testArray);
     // **** COMMENTED THE BELOW OUT TO NOT USE ALL OUR BING CALLS
     // BELOW STILL WORKS
     // BE SURE TO CHANGE WEBSITES UPDATE TO PROPER OBJECT KEYS
-    //webSitesUpdate(testArray);
+    // webSitesUpdate(testArray);
     return axios(config)
-      .then(function (response) {
-        //console.log(JSON.stringify(response));
+      .then((response) => {
+        // console.log(JSON.stringify(response));
         console.log(JSON.stringify(response.data[1]));
-        //console.log(JSON.stringify(response.data.webPages.value));
+        // console.log(JSON.stringify(response.data.webPages.value));
         webSitesUpdate(response.data[0].webPages.value);
         if (response.data[1] !== null) {
           reviewedSitesUpdate(response.data[1]);
@@ -34,66 +38,103 @@ function Search() {
           reviewedSitesUpdate(["We didn't find any matches"]);
         }
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
       });
   };
 
-  const [webSites, webSitesUpdate] = useState(['Search Results Appear Here']);
+  const [webSites, webSitesUpdate] = useState([]);
   const [reviewedSites, reviewedSitesUpdate] = useState([]);
   // const [userInfo, userInfoUpdate] = useState([
   //   { User: { username: 'hello' } },
   // ]);
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => searchBing(data);
+  const onSubmit = (data) => {
+    searchBing(data);
+    // boom = data.clientSearch;
+    // redir = <Redirect to="/searchresults" />;
+  };
+
+
+  // const tester = (data) => {
+  //   if(data){
+  //     if (count === 1) {
+  //       searchBing(data);
+  //     } else {
+  //       count++;
+  //       console.log(data);
+  //     }
+  //   } else {
+  //     console.log('no data');
+  //   }
+  // };
+
+  useEffect(() => {
+    // axios.get('/api/search').then(({ data }) => {
+    //    boom = data;
+    // });
+  }, []);
+
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Search:</label>
-        <input ref={register} name='clientSearch' />
+        <input ref={register} name="clientSearch" />
         {/* <Link
           to={{
             pathname: `/search`,
           }}
         >
-        
+
           <button>Search websites</button>
         </Link> */}
-        <button>Search websites</button>
-        <div className='reviewedSites list'>
-          {reviewedSites.map((review) => {
-            return (
-              <div key={review.id}>
-                <br></br>
-                {/* <div>Written By: {review.User.username}</div> */}
-                <div>Likes: {review.likes}</div>
-                <div> Dislikes: {review.dislike}</div>
-                <br></br>
-                <div>{review.text}</div>
-                <button>See Review</button>
+        {/* <Switch> */}
+          {/* <Route exact path="/"> */}
+              <button>Search websites</button>
+          {/* </Route> */}
+          {/* <Route exact path="/searchresults"> */}
+              {/* <button>Search websites</button> */}
+          {/* </Route> */}
+        {/* </Switch> */}
+        <div className="reviewedSites list">
+          {reviewedSites.map((review) => (
+            <div key={review.id}>
+              <br />
+              {/* <div>Written By: {review.User.username}</div> */}
+              <div>
+                Likes:
+                {review.likes}
               </div>
-            );
-          })}
+              <div>
+                {' '}
+                Dislikes:
+                {review.dislike}
+              </div>
+              <br />
+              <div>{review.text}</div>
+              <button>See Review</button>
+            </div>
+          ))}
         </div>
-        <div className='webSitesList'>
-          {webSites.map((site) => {
-            return (
-              <div key={site.id}>
-                <br></br>
-                <a href={site.url}>{site.url}</a>
-                <br></br>
-                <div>{site.snippet}</div>
-                <Link
-                  to={{
-                    pathname: `/review/site=${site.url}`,
-                  }}
-                >
-                  <button>Review Website!</button>
-                </Link>
-              </div>
-            );
-          })}
+        {/* {tester(boom)}
+        {redir} */}
+        <div className="webSitesList">
+          {webSites.map((site) => (
+            <div key={site.id}>
+              <br />
+              <a href={site.url}>{site.url}</a>
+              <br />
+              <div>{site.snippet}</div>
+              <Link
+                to={{
+                  pathname: `/review/site=${site.url}`,
+                }}
+              >
+                <button>Review Website!</button>
+              </Link>
+            </div>
+          ))}
         </div>
       </form>
     </div>
