49e699b1477ae92c3f2d07bef693d9a95299e4a1
benyoss <benyoss4@gmail.com>
2020-08-16 09:56:05 -0500

    "Polished up the layout and design of user profile, and segregated the inputs from the actual user data."

--- client/src/profile.jsx
+++ client/src/profile.jsx
@@ -1,11 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { styled, Backdrop } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Box from '@material-ui/core/Box';
 
 function Profile() {
   const [user, setUser] = useState([]);
   const { register, handleSubmit } = useForm();
+
+  const MyButton = styled(Button)({
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 20,
+    padding: '0 20px',
+  });
+
+  const Background = styled(Toolbar)({
+    background: 'linear-gradient(45deg, #FE6242 30%, #FF2445 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'red',
+  });
+
+  const ReviewBG = styled(Box)({
+    borderRadius: 3,
+    height: 200,
+    boxShadow: '0 3px 5px 2px #b81a06',
+    backgroundColor: '#FAEBD7',
+    color: 'black',
+  });
+
+  const ImageBG = styled(Box)({
+    borderRadius: 7,
+    boxShadow: '0 1px 30px 0px gray',
+    color: 'black',
+  });
 
   const onSubmit = (userBio) => {
     axios.post('/profile/bio', { bio: userBio }).then(({ data }) => {
@@ -37,7 +73,8 @@ function Profile() {
 
   return (
     <div>
-      <div style={{ backgroundColor: '#800000' }}>
+      <div>
+        <Background>
         <h1
           style={{
             display: 'inline-block',
@@ -59,29 +96,39 @@ function Profile() {
           </h1>
         </Link>
         <form onSubmit={handleSubmit(userLogout)}>
-        <button>Logout</button>
+        <button><MyButton>Logout</MyButton></button>
 
         </form>
+        </Background>
+       
       </div>
-      <img src={user.image} width='20%' height='20%' />
+      <ImageBG width="200">
+      <div >
+      <img src={user.image} style={{ position: 'absolute', marginBottom: "20px", }} width='150px'
+          height='150px'/>
+      <h2 style={{ marginLeft: "300px", padding: "0px"}}>Bio for {user.username}</h2>
+      <div style={{ maxWidth: "700px", marginLeft: "300px", marginBottom: "10px", positon: "absolute", padding: "20px" }}>{user.bio}</div>
+      <img height="10" style={{marginTop: "20px"}}></img>
+      </div>
+      </ImageBG>
+      <ReviewBG style={{ marginTop: "20px" }}>
+      <div style={{ display: 'inline-block' }}>
       <h3>Edit Image</h3>
       <form onSubmit={handleSubmit(imageSubmit)}>
         <textarea ref={register} name='imageUrl' />
-        <button>Submit Image</button>
+        <button><MyButton>Submit Image</MyButton></button>
       </form>
-      <div>
-        <div>
-          <h2>Bio for {user.username}</h2>
-          <div>{user.bio}</div>
-        </div>
       </div>
-      <div>
-        <div>
-          <h3>Edit Bio</h3>
+      <div style={{ display: 'inline-block', marginLeft: "200px" }}>
+      <h3>Edit Bio</h3>
           <form onSubmit={handleSubmit(onSubmit)}>
             <textarea ref={register} name='message' />
-            <button>Submit Bio</button>
+            <button><MyButton>Submit Bio</MyButton></button>
           </form>
+      </div>
+      </ReviewBG>
+      <div>
+        <div>
         </div>
       </div>
     </div>
