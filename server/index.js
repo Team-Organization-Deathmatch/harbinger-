d813dba54938c75b0d93854ff07e1618f527649c
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-15 22:25:21 -0500

    cleaned up code

--- server/index.js
+++ server/index.js
@@ -1,16 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const { app } = require('./app');
-// const app = express();
-// const route = require('./server');
+
 const PORT = process.env.PORT || 8000;
-// const path = require('path');
 
 app.listen(PORT, (req, res) => {
   console.log(`listening on ${PORT}`);
 });
-
-// app.use('/', express.static(path.join(__dirname, './server')));
-// module.exports = {
-//   app
-// }
--- server/routes/userProfile.js
+++ server/routes/userProfile.js
@@ -5,16 +5,10 @@ const userProfile = Router();
 
 userProfile.get('/:user', (req, res) => {
   let username = req.params.user;
-  // username is now equal to the user name
-  // I can query my db using this and retrieve
-  // their name
-  // here we will grab info from the DB for a single user profile and render it to the page
   if (req.user) {
     console.log(username);
     res.status(201);
     getUserReviews(username).then((data) => res.send(data));
-
-    //res.send('profile GET');
   } else {
     res.status(401);
     res.send('unauthorized');
