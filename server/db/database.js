b8a36a1d8a488de4f8460be353acd72a548d0485
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-18 11:46:08 -0500

    Test

--- server/db/database.js
+++ server/db/database.js
@@ -9,6 +9,7 @@ const db_pass = process.env.DB_Pass || '';
 const db_host = process.env.HOST || 'localhost';
 
 // Alternate between production db or local db
+// More comments
 const db = process.env.PRODENV === 'gcloud' ? new Sequelize(db_name, db_user, db_pass, {
   host: `/cloudsql/${process.env.HOST}`,
   dialect: 'mysql',
